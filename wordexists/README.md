f3e6a5ed02c35b48c22a58e0886671374883cc17
Ronald Bradford <ronald@doit-intl.com>
2023-05-24 12:53:20 -0400

    v2 of wordexists

---
+++ wordexists/README.md
@@ -0,0 +1,20 @@
+# Javascript code is 100% generated using chatGPT
+
+Example at: https://wordexists.ronald-ec2.workers.dev
+
+## v1
+
+* write me a cloudflare worker that can expose an api to return if a word exists in an existing list of words
+* can you add error handling and a http error page if input is not specified for this code
+* can you return a json response when no input is specified
+
+
+## v2
+
+* can you convert the wordlist to read from a file
+* be sure to lowercase the input and the list of words
+
+
+## Local copy of words for reference
+wget -O wordlist.txt https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words
+
--- wordexists/worker.js
+++ wordexists/worker.js
@@ -1,17 +1,3 @@
-// List of words
-const wordList = [
-  'apple',
-  'banana',
-  'cherry',
-  'date',
-  'elderberry',
-  'fig',
-  'grape',
-  'honeydew',
-  'kiwi',
-  'lemon'
-];
-
 // Handle incoming requests
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
@@ -22,7 +8,7 @@ async function handleRequest(request) {
   // Extract the word from the request URL
   const url = new URL(request.url);
   const path = url.pathname;
-  const word = path.substring(1); // Remove leading slash
+  const word = path.substring(1).toLowerCase(); // Remove leading slash and convert to lowercase
 
   if (!word) {
     // Input not specified, return JSON error response
@@ -38,10 +24,11 @@ async function handleRequest(request) {
     });
   }
 
+  // Read the word list from a file
+  const wordList = await getWordListFromFile();
 
-
-  // Check if the word exists in the list
-  const exists = wordList.includes(word);
+  // Check if the lowercase word exists in the lowercase list
+  const exists = wordList.map(w => w.toLowerCase()).includes(word);
 
   // Prepare the response
   const response = {
@@ -54,4 +41,22 @@ async function handleRequest(request) {
       'Content-Type': 'application/json'
     }
   });
+}
+
+// Helper function to read the word list from a file
+async function getWordListFromFile() {
+  // Fetch the file content
+  const response = await fetch('https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words'); // Replace with your actual file URL
+
+  if (response.ok) {
+    // Read the file content as text
+    const text = await response.text();
+
+    // Split the text by new lines to create the word list
+    const wordList = text.split('\n');
+
+    return wordList;
+  } else {
+    throw new Error('Failed to fetch word list');
+  }
 }
